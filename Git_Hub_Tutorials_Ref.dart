efae61cc2ea14f12c25c34d754ad43db597864de
unknown <quiry1111@gmail.com>
2024-03-07 02:23:21 +0200

    Git_Hub_Tutorials Ref

---
+++ Git_Hub_Tutorials_Ref.dart
@@ -0,0 +1,45 @@
+// git status
+/*  
+    git status 
+    git add path_file | *
+    git reset head file_name
+    git commit -m "message here"
+    git push remote_name  local_branch_name
+
+
+    git remote      -v    get remote_name Repo
+    git branch            get Branches on your local Repo 
+    git branch            branch_name_will_created   Create A Branch
+    git checkout          branch_name_will_comeInto   Go To Branch
+    git  checkout   -b    rename_branch   Create Branch And Go To It
+    git  branch     -m    rename_branch   Rename Brance You Are In
+    git  branch     -d    Delete_branch   Check First If Edits Exist
+    git  branch     -D    Delete_branch  Force Delete Without Check
+
+    Delete You Must Not Exist In It To Delete
+    Rename You Must Exist In It To Rename
+
+    
+
+    git config -- global --edit  To Configurate Or Set Settings In Editors
+    git config -l | --list       To List Configure
+    git config -l --show-origin  To Show From Where Git_Hub Get Data
+    git config help              Open Manual Of Git_Hub 
+    git config --global key.value => user.email  [Get] Settings In CommandLine
+    git config --global key.value => user.email "Value" [Set] Settings In CommandLine
+    git config --global key.value => user.email "" To Unset Setting | Make It Empty
+    git config --global --unset key.value          To Unset Setting | Make It Empty 
+
+    To Edit On Aliases In CommandLine
+      git config --global alias.shortcuts command | "command if there is a space"
+        => git config --global alias.st status 
+        => git config --global alias.cm "commit -m" 
+    To Edit In Editor
+      git config --global --edit => To Edit In Editor    
+    Search On => List Git Alis
+
+    
+
+
+
+*/
