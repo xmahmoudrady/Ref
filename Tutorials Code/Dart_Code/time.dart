1670ab9abfae319f42a3061ea1646630aeab7397
unknown <qu3ry1111@gmail.com>
2024-03-11 08:15:41 -0700

    other edit in ref-txt

--- Tutorials Code/Dart_Code/time.dart
+++ Tutorials Code/Dart_Code/time.dart
@@ -6,7 +6,7 @@ void main() {
   // var t3 = DateTime.now();
   var t4 = DateTime.now().timeZoneName;
   // var t1 = DateTime(2024);
-  print(t1);
+  print(t1);e
   print(t2);
   print(t2.isUtc);
   // print( "${t3}============\n");
---
+++ Tutorials Code/Test.dart
@@ -0,0 +1,5 @@
+void main() {
+    String name = "Wael";
+//   print(name * 3);
+print("i love you");
+}
